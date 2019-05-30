dd6e76a350a4a33ca27e076b407a76886d0a8b47
yuwen4514 <48576822+yuwen4514@users.noreply.github.com>
2019-05-31 03:35:37 +0800

    Add files via upload

---
+++ basic-homework21.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+struct servitor
+
+{
+
+   char m[8],q[12];
+
+   int g,w,h;
+
+};
+
+void calc(struct servitor *);
+
+int main(void)
+
+{
+
+   struct servitor emp; 
+
+   printf("                 Servitor工讀生結構體                 \n");
+
+   printf("-------------------------------------------------\n"); 
+
+   printf("請輸入編號,姓名,時薪,工作時數: ");
+
+   scanf(" %s %s %d %d", emp.m, emp.q, &emp.g, &emp.w);
+
+   calc(&emp);
+
+   printf("月收入: %d\n", emp.h);
+
+   system("pause"); 
+
+   return 0;
+
+}
+
+void calc(struct servitor *p)
+
+{
+    p->h=p->g*p->w;
+}
